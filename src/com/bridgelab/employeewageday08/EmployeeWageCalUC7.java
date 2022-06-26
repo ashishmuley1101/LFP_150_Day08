449500ee1b7a0a91516b258cc153554477b690a7
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-06-26 13:41:41 +0530

    Day 08 Employee wage using oops

---
+++ src/com/bridgelab/employeewageday08/EmployeeWageCalUC7.java
@@ -0,0 +1,9 @@
+package com.bridgelab.employeewageday08;
+
+public class EmployeeWageCalUC7 {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage Computation program...");
+	}
+
+}
